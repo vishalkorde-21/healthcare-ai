75268960cdb5691f1424ac8554c3de2788018ddd
Vishal korde <vishalkorde153@gmail.com>
2026-03-28 12:45:30 +0530

    final update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
     try {
       const response = await axios.post(
         "http://127.0.0.1:8000/create/",
-        data
+        form
       );
       setResult(response.data);
     } catch (error) {
